7bfdb383d93dda81b8b3758891a4655f5051f8b4
yuvi444 <112838629+yuvi444@users.noreply.github.com>
2023-01-10 13:25:49 +0530

    Add files via upload

---
+++ EX 5-AI.py
@@ -0,0 +1,19 @@
+a=[1,3,5,7,8]
+print(len(a))
+
+b="i am"
+c="\tartist"
+print(b+c)
+
+d=[13,54,64,6]
+print(100 not in d)
+print(13 in d)
+print(20 in d)
+
+e=[2,3,4,5,6]
+print(e[2])
+print(e[-3])
+print(e[0:3])
+
+f=[8,7,6,5]
+print(f[0:3])
